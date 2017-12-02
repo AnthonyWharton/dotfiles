ae4e8b38646874b6857557b814da1adc5e48bb46
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2017-12-02 19:34:14 +0000

    Fixed Rofi

---
+++ i3/.config/i3/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -show drun -lines 7 -color-enabled -color-window "#2f343f, #002b36, #000000" -color-normal "#2f343f, #5d6371, #2c313c, #5d6371, #f3f4f5" -opacity "100" -bw 1 -font "Roboto Mono Light 32"
