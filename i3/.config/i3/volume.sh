39e2416173e92fe33e623ec52a667b98f79995fd
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-08-22 18:16:04 +0100

    Fix volume script to always work to the nearest 5%

--- i3/.config/i3/volume.sh
+++ i3/.config/i3/volume.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
-# Changes the system's volume level.
-# Not the most general implementation, for use with my i3 config.
+# Changes the system's volume level to the next 5% increment.
+# Not the most general implementation, for use with my personal i3 config.
 
-VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
-MUTE=$(pactl list sinks | grep '^[[:space:]]Mute:' | sed -e 's/.*\(yes\|no\).*/\1/')
+VOL_MAX=$(pactl list sinks | grep "^[[:space:]]Base Volume:" | sed -E -e 's/^[^0-9]*([0-9]*).*$/\1/')
+VOL_CUR=$(pactl list sinks | grep "^[[:space:]]Volume:"      | sed -E -e 's/^[^0-9]*([0-9]*).*$/\1/')
+IS_MUTE=$(pactl list sinks | grep "^[[:space:]]Mute:"        | sed -E -e 's/.*(yes|no).*/\1/')
+VOL_STP=$(($VOL_MAX / 20 + 1))
 
+# Finds the higher and lower values for 5% volume increments around the current
+# volume. This is used so the volume_up and volume_down functions can always
+# align the volume to 5% increments perfectly, regardless on the current volume.
+I_L=0
+I_H=$VOL_STP
+while [ ! $I_L -le $VOL_CUR -o ! $VOL_CUR -lt $I_H ]; do
+    I_L=$(($I_L + $VOL_STP))
+    I_H=$(($I_H + $VOL_STP))
+done
+
+# Increases the volume to the next 5% volume increment
 function volume_up {
-    if [[ $VOLUME -lt 100 ]]; then
-        if [[ "$MUTE" = "yes" ]]; then
+    if [[ $VOL_CUR -lt $VOL_MAX ]]; then
+        if [[ "$IS_MUTE" = "yes" ]]; then
             toggle_mute
         fi
-        pactl set-sink-volume 0 +3277        
+	pactl set-sink-volume 0 +$(($I_H - $VOL_CUR))
     fi
 }
 
+# Decreases the volume to the next 5% volume increment
 function volume_down {
-    if [[ $VOLUME -gt 0 ]]; then 
-        if [[ "$MUTE" = "yes" ]]; then
+    if [[ $VOL_CUR -gt 0 ]]; then 
+        if [[ "$IS_MUTE" = "yes" ]]; then
             toggle_mute
         fi
-        pactl set-sink-volume 0 -3277
+        if [ $VOL_CUR -eq $I_L ]; then
+            pactl set-sink-volume 0 -$VOL_STP
+	else
+            pactl set-sink-volume 0 -$(($VOL_CUR - $I_L))
+	fi
     fi
 }
 
