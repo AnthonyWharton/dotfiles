203d1383724f918973f6583813359a607ff24be4
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-08-30 19:42:45 +0100

    Replace lock script
     Note to self: Don't remove this!

---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Note to self: Don't delete this file!
+# (This is in a script so that it can be consistent between the i3 hotkey and 
+# the systemd task for when shutting the lid.)
+i3lock-next "" "SauceCodePro Nerd Font" 32
