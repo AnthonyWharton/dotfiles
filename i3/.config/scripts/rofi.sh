68529637f8c4a7cce1634afefb43964e3c07981c
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2018-12-13 13:16:27 +0000

    Add WIP new rofi launch script

--- i3/.config/scripts/rofi.sh
+++ i3/.config/scripts/rofi.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-rofi -show drun -lines 7 -color-enabled -color-window "#2f343f, #002b36, #000000" -color-normal "#2f343f, #5d6371, #2c313c, #5d6371, #f3f4f5" -opacity "100" -bw 1 -font "Roboto Mono Light 18"
+rofi -modi drun,ssh \
+	-show drun \
+	-ssh-command "WINIT_HIDPI_FACTOR=1.0 alacritty" \
+	-lines 7 \
+	-color-enabled \
+	-color-window "#2f343f, #002b36, #000000" \
+	-color-normal "#2f343f, #5d6371, #2c313c, #7fb800, #f3f4f5" \
+	-opacity "100" \
+	-bw 0 \
+	-font "SauceCodePro Nerd Font 16"
