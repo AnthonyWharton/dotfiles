2e55c8cb0df059ca37892334cbbd00eaf4bbb828
Anthony Wharton <a.wharton.2015@bristol.ac.uk>
2019-01-03 15:43:59 +0000

    Fox volume script to be *slightly* more robust to multiple audio sinks

--- i3/.config/scripts/volume.sh
+++ i3/.config/scripts/volume.sh
@@ -2,19 +2,19 @@
 # Changes the system's volume level to the next 5% increment.
 # Not the most general implementation, for use with my personal i3 config.
 
-VOL_MAX=$(pactl list sinks | grep "^[[:space:]]Base Volume:" | sed -E -e 's/^[^0-9]*([0-9]*).*$/\1/')
-VOL_CUR=$(pactl list sinks | grep "^[[:space:]]Volume:"      | sed -E -e 's/^[^0-9]*([0-9]*).*$/\1/')
-IS_MUTE=$(pactl list sinks | grep "^[[:space:]]Mute:"        | sed -E -e 's/.*(yes|no).*/\1/')
-VOL_STP=$(($VOL_MAX / 20 + 1))
+VOL_MAX=$(pactl list sinks | grep "^[[:space:]]Base Volume:" | sed -E -e 's/^[^0-9]*([0-9]*).*$/\1/' | head -n 1)
+VOL_CUR=$(pactl list sinks | grep "^[[:space:]]Volume:"      | sed -E -e 's/^[^0-9]*([0-9]*).*$/\1/' | head -n 1)
+IS_MUTE=$(pactl list sinks | grep "^[[:space:]]Mute:"        | sed -E -e 's/.*(yes|no).*/\1/' | head -n 1)
+VOL_STP=$((VOL_MAX / 20 + 1))
 
 # Finds the higher and lower values for 5% volume increments around the current
 # volume. This is used so the volume_up and volume_down functions can always
 # align the volume to 5% increments perfectly, regardless on the current volume.
 I_L=0
 I_H=$VOL_STP
-while [ ! $I_L -le $VOL_CUR -o ! $VOL_CUR -lt $I_H ]; do
-    I_L=$(($I_L + $VOL_STP))
-    I_H=$(($I_H + $VOL_STP))
+while [[ ! $I_L -le $VOL_CUR || ! $VOL_CUR -lt $I_H ]]; do
+    I_L=$((I_L + VOL_STP))
+    I_H=$((I_H + VOL_STP))
 done
 
 # Increases the volume to the next 5% volume increment
@@ -23,20 +23,20 @@ function volume_up {
         if [[ "$IS_MUTE" = "yes" ]]; then
             toggle_mute
         fi
-	pactl set-sink-volume 0 +$(($I_H - $VOL_CUR))
+	pactl set-sink-volume 0 +$((I_H - VOL_CUR))
     fi
 }
 
 # Decreases the volume to the next 5% volume increment
 function volume_down {
-    if [[ $VOL_CUR -gt 0 ]]; then 
+    if [[ $VOL_CUR -gt 0 ]]; then
         if [[ "$IS_MUTE" = "yes" ]]; then
             toggle_mute
         fi
-        if [ $VOL_CUR -eq $I_L ]; then
+        if [[ $VOL_CUR -eq $I_L ]]; then
             pactl set-sink-volume 0 -$VOL_STP
 	else
-            pactl set-sink-volume 0 -$(($VOL_CUR - $I_L))
+            pactl set-sink-volume 0 -$((VOL_CUR - I_L))
 	fi
     fi
 }
@@ -54,7 +54,7 @@ function print_help {
     echo "    -t | --toggle   Toggles system muted status on/off."
     echo "    -h | --help     Prints this help dialogue and exits."
 }
- 
+
 if [[ $# -eq 0 ]]; then
     print_help
     echo "No arguments given. Exiting.."
